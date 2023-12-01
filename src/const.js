262f96c7167e90fda98919246fbf7a82608e11ba
Aakarsh Garg <agarg6ag@gmail.com>
2023-12-01 23:54:44 +0530

    no

--- src/const.js
+++ src/const.js
@@ -1,2 +1,6 @@
-const BASE_URL ='http://192.168.0.107:3000'
+const BASE_URL ='http://192.168.0.101:3000'
 export default BASE_URL
+
+//Aakarsh's IdeaPad Gaming 3
+//192.168.0.101 reserved at Home, Aditya's Home
+//192.168.29.100 reserved at Arnav's Home
