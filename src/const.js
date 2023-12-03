33ba6dafc8f09b91693a007922165c6b2268438d
Aakarsh Garg <agarg6ag@gmail.com>
2023-12-03 17:01:27 +0530

    yesnt

--- src/const.js
+++ src/const.js
@@ -1,6 +1,6 @@
 const BASE_URL ='http://192.168.0.101:3000'
 export default BASE_URL
 
-//Aakarsh's IdeaPad Gaming 3
+//Aakarsh's MacBook
 //192.168.0.101 reserved at Home, Aditya's Home
 //192.168.29.100 reserved at Arnav's Home
